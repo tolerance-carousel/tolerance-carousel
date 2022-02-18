8acb3d044ed3329de5bb0789389f508c991014d1
Simon <Simon-Dirks@users.noreply.github.com>
2022-02-18 12:17:48 +0100

    Add video.js basic video playback.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,48 @@
 <template>
-
+  <video ref="videoPlayer" class="video-js"></video>
 </template>
 
-<script setup lang="ts">
+<script>
+import videojs from "video.js";
 
+export default {
+  name: 'HelloWorld',
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {
+      player: null,
+      videoOptions: {
+        autoplay: false,
+        controls: true,
+        // fluid: true,
+        sources: [
+          {
+            src:
+                "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
+            type: "video/mp4"
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    this.player = videojs(this.$refs.videoPlayer, this.videoOptions, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    })
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+}
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import '../node_modules/video.js/dist/video-js.css'
 
 createApp(App).mount('#app')
