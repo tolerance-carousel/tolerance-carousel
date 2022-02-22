5f6cf808cfb9b6433597fcf6c923ab1823486d11
Simon <Simon-Dirks@users.noreply.github.com>
2022-02-22 11:16:20 +0100

    Embed Polis.

--- src/components/Polis.vue
+++ src/components/Polis.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-    <p>POLIS PART</p>
+  <div class="grid grid-cols-2 gap-2">
+    <div>
+      <p>POLIS</p>
+    </div>
+    <div class='polis' data-conversation_id='3bbxvmcwdc'></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Polis"
+  name: "Polis",
+  mounted() {
+    // TODO: Host embed script locally, and add styling/layout changes there.
+    let polisEmbedScript = document.createElement('script');
+    polisEmbedScript.setAttribute('src', 'https://pol.is/embed.js');
+    document.head.appendChild(polisEmbedScript);
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
 </style>
