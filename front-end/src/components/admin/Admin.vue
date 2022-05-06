9a9fd2aa672be4c88c8c0be74d933a33bc14f36b
Simon <Simon-Dirks@users.noreply.github.com>
2022-05-06 10:13:37 +0200

    Style Admin panel

--- front-end/src/components/admin/Admin.vue
+++ front-end/src/components/admin/Admin.vue
@@ -2,28 +2,28 @@
   <div class="p-4">
     <p class="font-bold mb-3">ADMIN PANEL</p>
     <div>
-      <button @click="startVideo('room_1')">Start room_1 video</button>
+      <button class="bg-slate-600 hover:bg-slate-700 rounded-full text-white px-3 " @click="startVideo('room_1')">Start room_1 video</button>
     </div>
     <div>
-      <button @click="resetVideo('room_1')">Reset room_1 video</button>
+      <button class="bg-slate-600 hover:bg-slate-700 rounded-full text-white px-3 mt-1" @click="resetVideo('room_1')">Reset room_1 video</button>
     </div>
 
     <div class="mt-4">
-      <button @click="startVideo('room_2')">Start room_2 video</button>
+      <button class="bg-slate-600 hover:bg-slate-700 rounded-full text-white px-3 mt-1" @click="startVideo('room_2')">Start room_2 video</button>
     </div>
     <div>
-      <button @click="resetVideo('room_2')">Reset room_2 video</button>
+      <button class="bg-slate-600 hover:bg-slate-700 rounded-full text-white px-3 mt-1" @click="resetVideo('room_2')">Reset room_2 video</button>
     </div>
 
     <div class="mt-4">
-      <button @click="startVideo('room_3')">Start room_3 video</button>
+      <button class="bg-slate-600 hover:bg-slate-700 rounded-full text-white px-3 mt-1" @click="startVideo('room_3')">Start room_3 video</button>
     </div>
     <div>
-      <button @click="resetVideo('room_3')">Reset room_3 video</button>
+      <button class="bg-slate-600 hover:bg-slate-700 rounded-full text-white px-3 mt-1" @click="resetVideo('room_3')">Reset room_3 video</button>
     </div>
 
     <hr class="mt-5">
-
+    <p class="mt-5">To use the admin buttons above, please enter the password in the field below first.</p>
     <PasswordInput :hide-label="true"/>
   </div>
 
