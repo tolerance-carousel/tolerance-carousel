197202e355ca1c47d5ad52908622421e31f33a0b
Simon <Simon-Dirks@users.noreply.github.com>
2022-05-06 11:20:46 +0200

    Increase size of QR code on screen

--- front-end/src/components/video-player/SharePerspectiveLink.vue
+++ front-end/src/components/video-player/SharePerspectiveLink.vue
@@ -1,11 +1,11 @@
 <template>
   <img :src="`/qr-codes/${roomId}.png`"
        :alt="`QR Code`"
-       class="mx-auto rounded-md" :class="[showAsVideoOverlay ? 'w-24' : '']">
+       class="mx-auto rounded-md" :class="[showAsVideoOverlay ? 'w-32 mb-1' : '']">
 
   <div class="text-center">
     <a :href="`/share-perspective/${roomId}`"
-       :class="[showAsVideoOverlay ? 'text-sm rounded-sm px-2 py-[0.1rem] bg-[rgba(255,255,255,0.75)]' : 'text-2xl']">
+       :class="[showAsVideoOverlay ? 'text-lg rounded-sm px-2 py-[0.1rem] bg-[rgba(255,255,255,0.75)]' : 'text-2xl']">
         <span v-if="roomId==='room_1'">
           https://edu.nl/xuu33
         </span>
