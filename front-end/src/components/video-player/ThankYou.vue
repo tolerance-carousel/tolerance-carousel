46fae207069ffa871a50d241bab60107bfc28c5b
Simon <Simon-Dirks@users.noreply.github.com>
2022-05-23 09:02:27 +0200

    Update credits

--- front-end/src/components/video-player/ThankYou.vue
+++ front-end/src/components/video-player/ThankYou.vue
@@ -1,56 +1,72 @@
 <template>
-  <div class="text-center pt-8 bg-[#f4f2f0] text-[#111111] h-screen">
-    <h1 class="text-4xl font-bold">
-      Dank je wel voor het meedoen!
-    </h1>
-    <p class="mt-4 text-lg">
-      Vragen, gedachten of opmerkingen? Voel je vrij om iemand in je omgeving of een van de facilitators te benaderen.
-    </p>
+  <div class="text-center pt-8 bg-[#f4f2f0] text-[#111111] h-screen flex flex-col">
+    <div style="flex: 1 1 auto" class="px-4">
+      <h1 class="text-4xl font-bold">
+        Dank je wel voor het meedoen!
+      </h1>
+      <p class="mt-4 text-lg">
+        Vragen, gedachten of opmerkingen? Voel je vrij om iemand in je omgeving of een van de facilitators te benaderen.
+      </p>
 
-    <div class="grid grid-cols-3 px-8 mt-8">
-      <div class="mt-8 px-8">
-        <img src="/stills/maria-ruysch.jpg" alt="Maria Ruysch" class="h-64 inline-block rounded-2xl drop-shadow">
-        <p class="text-lg mt-2">Maria Ruysch</p>
-      </div>
-      <div class="mt-8 px-8">
-        <img src="/stills/abraham-aboab.jpg" alt="Abraham Aboab" class="h-64 inline-block rounded-2xl drop-shadow">
-        <p class="text-lg mt-2">Abraham Aboab</p>
-      </div>
-      <div class="mt-8 px-8">
-        <img src="/stills/condoomverkoopster.jpg" alt="Condoomverkoopster"
-             class="h-64 inline-block rounded-2xl drop-shadow">
-        <p class="text-lg mt-2">Condoomverkoopster</p>
+      <div class="">
+        <button @click="onResetShow()"
+                class="bg-red-800 rounded-full text-white px-3 opacity-0 hover:opacity-100 transition-opacity">
+          Reset naar welkomstscherm
+        </button>
+
+        <div class="text-black opacity-0 hover:opacity-100 transition-opacity inline-block ml-4">
+          <PasswordInput :hide-label="true"/>
+        </div>
       </div>
 
+      <div class="grid grid-cols-1 md:grid-cols-3 px-8 mb-8">
+        <div class="mt-8 px-8">
+          <img src="/stills/maria-ruysch.jpg" alt="Maria Ruysch" class="max-h-64 inline-block rounded-2xl drop-shadow">
+          <p class="text-lg mt-2">Maria Ruysch</p>
+        </div>
+        <div class="mt-8 px-8">
+          <img src="/stills/abraham-aboab.jpg" alt="Abraham Aboab" class="max-h-64 inline-block rounded-2xl drop-shadow">
+          <p class="text-lg mt-2">Abraham Aboab</p>
+        </div>
+        <div class="mt-8 px-8">
+          <img src="/stills/condoomverkoopster.jpg" alt="Condoomverkoopster"
+               class="max-h-64 inline-block rounded-2xl drop-shadow">
+          <p class="text-lg mt-2">Condoomverkoopster</p>
+        </div>
+
+      </div>
     </div>
+
+    <footer class="bg-[#d60034] w-screen text-left py-4" style="flex: 0 1 40px;">
+      <h1 class="font-bold text-lg mx-8">
+        Met dank aan
+      </h1>
+
+      <div class="my-4 mx-8 text-lg columns-2">
+        <p><strong>Zenzi Alwart</strong> Maria Ruysch</p>
+        <p><strong>Noël van Santen</strong> Abraham Aboab</p>
+        <p><strong>Charlotte vd Linden</strong> condoomverkoopster</p>
+        <p><strong>Wouter Eizenga</strong> regisseur</p>
+        <p><strong>Jeanette van Vliet</strong> kledingstyliste</p>
+        <p><strong>Boris Everts</strong> filmmaker</p>
+        <p><strong>Servaas Everts</strong> locatie scout</p>
+        <p><strong>Simon Dirks</strong> IT specialist</p>
+        <p><strong>Tim Overkempe</strong> historisch onderzoek</p>
+        <p><strong>Shahin Nazar</strong> tolerantie onderzoeker</p>
+        <p><strong>Toine Pieters</strong> manusje van alles</p>
+        <p><strong>Gekomen om te blijven</strong> (U900) en <strong>HUA</strong> voor inhoudelijke ondersteuning</p>
+        <p><strong>Provincie Utrecht</strong> voor financiële ondersteuning</p>
+      </div>
+      <!--    <span>-->
+      <!--      <img src="/logos/logo-hua.svg" alt="Logo van Het Utrechts Archief" class="w-8 md:w-24 inline-block mr-8">-->
+      <!--      <img src="/logos/logo-uu.svg" alt="Logo van Universiteit Utrecht" class="w-8 md:w-24 inline-block mr-8">-->
+      <!--      <img src="/logos/logo-utrecht.svg" alt="Logo van Gemeente Utrecht" class="w-8 md:w-24 inline-block mr-8">-->
+      <!--    </span>-->
+
+    </footer>
   </div>
 
 
-  <footer class="fixed bottom-0 bg-[#d60034] w-screen text-center py-4">
-    <!--    <h1 class="font-bold text-lg">-->
-    <!--      Partners-->
-    <!--    </h1>-->
-    <!--    <span class="mb-4 mr-8 text-lg">-->
-    <!--      In samenwerking met...-->
-    <!--    </span>-->
-    <span>
-      <img src="/logos/logo-hua.svg" alt="Logo van Het Utrechts Archief" class="h-16 inline-block mr-8">
-      <img src="/logos/logo-uu.svg" alt="Logo van Universiteit Utrecht" class="h-16 inline-block mr-8">
-      <img src="/logos/logo-utrecht.svg" alt="Logo van Gemeente Utrecht" class="h-16 inline-block mr-8">
-    </span>
-
-    <div class="absolute right-4 top-4">
-      <button @click="onResetShow()"
-              class="bg-red-800 rounded-full text-white px-3 opacity-50 hover:opacity-100 transition-opacity">
-        Reset naar welkomstscherm
-      </button>
-
-      <div class="text-black opacity-50 hover:opacity-100 transition-opacity">
-        <PasswordInput :hide-label="true"/>
-      </div>
-    </div>
-
-  </footer>
 </template>
 
 <script>
